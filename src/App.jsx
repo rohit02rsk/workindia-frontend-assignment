3a8babeab44118777adccacd267991b0b12c5773
S Rohit Kumar <rohitkumar.s2020@vitstudent.ac.in>
2023-09-10 10:13:39 +0530

    done with design, fixing bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,49 @@
 import "./App.css";
+import sampleData from "./sampleData.json";
+import Card from "./components/Card";
 
 function App() {
-  return <div className="App">elo</div>;
+  const data = sampleData.data;
+
+  return (
+    <>
+      <div className="flex p-5">
+        <div className="w-1/3 m-2">
+          <h1 className="font-bold text-4xl">Applied</h1>
+          <div className="flex flex-col items-center pt-2 gap-3">
+            {data.map((row, i) => {
+              // console.log(row);
+              if (row.status === "Applied") {
+                return <Card key={i} details={row} />;
+              }
+            })}
+          </div>
+        </div>
+        <div className="w-1/3 m-2">
+          <h1 className="font-bold text-4xl">Accepted</h1>
+          <div className="flex flex-col items-center pt-2 gap-3">
+            {data.map((row, i) => {
+              // console.log(row);
+              if (row.status === "Accepted") {
+                return <Card key={i} details={row} />;
+              }
+            })}
+          </div>
+        </div>
+        <div className="w-1/3 m-2">
+          <h1 className="font-bold text-4xl">Rejected</h1>
+          <div className="flex flex-col items-center pt-2 gap-3">
+            {data.map((row, i) => {
+              // console.log(row);
+              if (row.status === "Rejected") {
+                return <Card key={i} details={row} />;
+              }
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
